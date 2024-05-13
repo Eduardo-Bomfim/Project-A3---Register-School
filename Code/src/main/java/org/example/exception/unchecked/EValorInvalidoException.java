49ea61478c73414ace5feed10b241fc4fe080900
Eduardo <eduardo.a.junior.dev@gmail.com>
2024-05-13 00:49:56 -0300

    create class runtime exception EValorInvalidoException

---
+++ Code/src/main/java/org/example/exception/unchecked/EValorInvalidoException.java
@@ -0,0 +1,8 @@
+package org.example.exception.unchecked;
+
+public class EValorInvalidoException extends RuntimeException {
+
+    public EValorInvalidoException(String message) {
+        super(message);
+    }
+}
