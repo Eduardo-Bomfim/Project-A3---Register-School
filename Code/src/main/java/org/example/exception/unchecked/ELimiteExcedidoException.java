f6736bc435ebcb662cd5affa45a6225519253151
Eduardo <eduardo.a.junior.dev@gmail.com>
2024-05-25 22:46:32 -0300

    create ELimiteExcedidoException

---
+++ Code/src/main/java/org/example/exception/unchecked/ELimiteExcedidoException.java
@@ -0,0 +1,8 @@
+package org.example.exception.unchecked;
+
+public class ELimiteExcedidoException extends RuntimeException{
+
+    public ELimiteExcedidoException(String message) {
+        super(message);
+    }
+}
