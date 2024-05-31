b9943e35d81ac0766da1e47ea8a5dcedbcb2b7d5
Eduardo <eduardo.a.junior.dev@gmail.com>
2024-05-31 17:44:37 -0300

    add method definirValoresAulas and getValorAula

--- Code/src/main/java/org/example/entidades/professor/Professor.java
+++ Code/src/main/java/org/example/entidades/professor/Professor.java
@@ -1,18 +1,22 @@
 package org.example.entidades.professor;
 
 import org.example.entidades.usuario.Usuario;
+import org.example.utilitarios.Valores;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Professor extends Usuario {
 
     private String titulacao;
+    private Valores valores;
 
     public Professor(long id, long registroAcademico, String nome, String email, String senha, String titulacao) {
         super(id, registroAcademico, nome, email, senha);
         this.titulacao = titulacao;
+        valores = new Valores(0, 0);
     }
 
+    //Getters e Setters
     public String getTitulacao() {
         return titulacao;
     }
@@ -21,19 +25,25 @@ public class Professor extends Usuario {
         this.titulacao = titulacao;
     }
 
-    public List consultarDisciplinasMinistradas(){
+    //Métodos
+
+    public ArrayList consultarDisciplinasMinistradas() {
         return null;
     }
 
-    public void definirValoresAulas(){
-
+    public void definirValoresAulas(int horas, double valor) {
+        valores.valorPorAula(horas, valor);
     }
 
-    public List consultarGruposDisponiveis(){
+    public Valores getValorAula() {
+        return valores;
+    }
+
+    public ArrayList consultarGruposDisponiveis() {
         return null;
     }
 
-    public List consultarAlunosInscritos(){
+    public ArrayList consultarAlunosInscritos() {
         return null;
     }
 
