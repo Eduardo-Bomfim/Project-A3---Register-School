f5d292609d4d832c3e853f06e464ebdff50d8d90
Eduardo <eduardo.a.junior.dev@gmail.com>
2024-06-06 21:52:15 -0300

    Create enum Area

---
+++ Code/src/main/java/org/example/entidades/coordenador/Area.java
@@ -0,0 +1,11 @@
+package org.example.entidades.coordenador;
+
+//define as áreas de atuação possíveis para professores e coordenadores
+public enum Area {
+
+    CIENCIAS_HUMANAS,
+    CIENCIAS_DA_NATUREZA,
+    LINGUAGENS,
+    EXATAS
+
+}
